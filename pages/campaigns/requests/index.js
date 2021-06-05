c2fb56591abbd9722a8892de64cbb247a528ebb8
Yash Uppal <officialyashuppal@gmail.com>
2021-06-05 18:18:05 +0530

    added Request List page routing

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class RequestIndex extends Component {
-    render() {
-        return (
-            <h3>Request List</h3>
-        );
-    }
+  render() {
+    return <h3>Request List</h3>;
+  }
 }
+
+export default RequestIndex;
