14fd81cd8694ab6a7bb95771ab55fc58b7bd7086
Yash Uppal <officialyashuppal@gmail.com>
2021-06-06 14:17:12 +0530

    Finished new request page and initialized request list page

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,27 +1,35 @@
 import React, { Component } from "react";
-import Layout from '../../../components/Layout';
-import { Button } from 'semantic-ui-react';
-import { Link } from '../../../routes';
+import { Button } from "semantic-ui-react";
+import { Link } from "../../../routes";
+import Layout from "../../../components/Layout";
+import Campaign from "../../../ethereum/campaign";
 
 class RequestIndex extends Component {
   static async getInitialProps(props) {
-    const {address} = props.query;
-    return {address};
-  }
+    const { address } = props.query;
+    const campaign = Campaign(address);
+    const requestCount = await campaign.methods.getRequestsCount().call();
 
+    const requests = await Promise.all(
+      Array(parseInt(requestCount))
+        .fill()
+        .map((element, index) => {
+          return campaign.methods.requests(index).call();
+        })
+    );
+    return { address, requests, requestCount };
+  }
   render() {
     return (
       <Layout>
-        <h3>Request List</h3>
+        <h3>Requests</h3>
         <Link route={`/campaigns/${this.props.address}/requests/new`}>
           <a>
-            <Button primary>
-            Add Request
-            </Button>          
+            <Button primary>Add Request</Button>
           </a>
         </Link>
       </Layout>
-    )
+    );
   }
 }
 
